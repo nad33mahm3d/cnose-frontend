9b734b93d1c829b89777f1095a52e0e0f651b88e
Nadeem Ahmed <ebox.nadeem@gmail.com>
2025-12-19 02:01:16 +0500

    update api base url

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios'
 import type { Vehicle, DeviceSnapshot, Alert } from '../types'
 
-const API_BASE_URL = '' // Use relative URLs to go through proxy
+const API_BASE_URL = 'api' // Use relative URLs to go through proxy
 
 const api = axios.create({
   baseURL: API_BASE_URL,
